de73df4ab1e9bf8177e1f6c38198cfd0b2973010
Mahesh Sundaram <mahesh@omkara.dev>
2022-06-05 07:29:48 -1000

    Update paths

--- dots/fzf.bash
+++ dots/fzf.bash
@@ -1,27 +1,27 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *$HOME/src/github.com/junegunn/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}$HOME/src/github.com/junegunn/fzf/bin"
+if [[ ! "$PATH" == *$HOME/code/junegunn/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/code/junegunn/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$HOME/src/github.com/junegunn/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/code/junegunn/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "$HOME/src/github.com/junegunn/fzf/shell/key-bindings.bash"
+source "$HOME/code/junegunn/fzf/shell/key-bindings.bash"
 
 # Options
 export FZF_DEFAULT_OPTS='--height 90% --border --color=light'
 
-# export FZF_DEFAULT_COMMAND='fd --type f'
+export FZF_DEFAULT_COMMAND='fd --type f'
 # export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # TAB starts completion
 export FZF_COMPLETION_TRIGGER='.'
 
-# npm install -g fd-find
+# cargo install fd-find
 # Use fd instead of the default find
 _fzf_compgen_path() {
   fd --hidden --follow --exclude ".git" --exclude node_modules/ . "$1"
